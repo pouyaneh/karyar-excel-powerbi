eeecb8adfa7902f93fd2db8e5915e585de2f1a55
pouyandeh <pouyandehpour@gmail.com>
2025-03-13 09:09:53 +0330

    hw15

---
+++ week15/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿-- ایجاد جدول جدید و کپی کردن داده‌ها
+SELECT * INTO product_test FROM products;
+-- تغییر مقادیر کالاهای تکراری
+UPDATE product_test SET productName = 'Chai' WHERE productID IN (1, 11, 21);
+UPDATE product_test SET productName = 'Chang' WHERE productID IN (2, 12, 22);
+-- حذف کالاهای تکراری و نگه داشتن کالایی با productID بزرگتر
+DELETE FROM product_test
+WHERE productID NOT IN (
+    SELECT MAX(productID)
+    FROM product_test
+    GROUP BY productName );
+--- تست کردن درستی جواب
+SELECT productName, COUNT(*) as duplicate_count
+FROM product_test
+GROUP BY productName
+HAVING COUNT(*) > 1
