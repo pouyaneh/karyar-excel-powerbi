023f4797a749048f3159bb21f41ffadfb716cd5b
pouyandeh <pouyandehpour@gmail.com>
2025-04-13 11:59:56 +0330

    hw17-5

---
+++ week17/SQLQuery5.sql
@@ -0,0 +1,14 @@
+﻿WITH CountrySales AS (
+    -- محاسبه مجموع فروش برای هر کشور
+    SELECT 
+        c.Country,
+        SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS TotalSales
+    FROM Orders o
+    JOIN [Order Details] od ON o.OrderID = od.OrderID
+    JOIN Customers c ON o.CustomerID = c.CustomerID
+    GROUP BY c.Country
+    HAVING c.Country IS NOT NULL
+)
+SELECT Country, TotalSales
+FROM CountrySales
+ORDER BY TotalSales ASC;  -- مرتب‌سازی بر اساس کمترین فروش
