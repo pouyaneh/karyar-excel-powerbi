317848c67a43fbe869cf7bb4f39641b4731c06b8
pouyandeh <pouyandehpour@gmail.com>
2025-05-14 07:00:30 +0330

    hw19-2

---
+++ week19/تابع پارامتر.sql
@@ -0,0 +1,54 @@
+create function dbo.getcustomersaleswithproduct
+(@startdate date,
+@enddate date
+)
+returns table
+as 
+return(
+SELECT 
+    c.CustomerID,
+    p.FirstName + ' ' + p.LastName AS CustomerName,
+    
+    -- ??????? ???????? ?????
+    st.Name AS TerritoryName,
+    st.CountryRegionCode,
+
+    -- ??????? ?????????? ?? ??? ????? ??? ?????? ????
+    sp.BusinessEntityID AS SalesPersonID,
+    p2.FirstName + ' ' + p2.LastName AS SalesPersonName,
+
+    -- ??????? ????? ?????
+    soh.OrderDate,
+
+    -- ??????? ?????
+    soh.SalesOrderID AS OrderID,
+    
+    -- **????? ????? (Order Status)**
+    soh.Status AS OrderStatus,
+
+    -- ??????? ?????
+    pr.ProductID,
+    pr.Name AS ProductName,
+    pc.Name AS ProductCategory,
+
+    -- ????? ???? ?? ???? ?? ??? ?????
+    soh.TotalDue AS TotalSales,
+
+    -- ????? ????? ??????????? ?? ??? ?????
+    sod.OrderQty AS QuantityPurchased
+
+FROM Sales.Customer c
+JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product pr ON sod.ProductID = pr.ProductID
+JOIN Production.ProductSubcategory psc ON pr.ProductSubcategoryID = psc.ProductSubcategoryID
+JOIN Production.ProductCategory pc ON psc.ProductCategoryID = pc.ProductCategoryID
+JOIN Sales.SalesTerritory st ON soh.TerritoryID = st.TerritoryID
+LEFT JOIN Sales.SalesPerson sp ON soh.SalesPersonID = sp.BusinessEntityID
+LEFT JOIN HumanResources.Employee e ON sp.BusinessEntityID = e.BusinessEntityID
+LEFT JOIN Person.Person p2 ON e.BusinessEntityID = p2.BusinessEntityID
+where soh.OrderDate between @startdate and @enddate
+)
+
+
